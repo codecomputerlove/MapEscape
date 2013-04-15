eae935cd90138e7f1ecb380485442c3a6b9914d6
Andrew Brandwood <andrew.brandwood@codecomputerlove.com>
2013-04-15 15:55:04 +0100

    Added files

---
+++ website/_client/scripts/core.js
@@ -0,0 +1,24 @@
+﻿(function($){
+
+	Core = {
+    init: function(){
+      $('.social-share').find('.btn').on('click', $.proxy(this, 'showShareBtn'));
+    },
+
+    showShareBtn: function(e){
+      e.preventDefault();
+      var $btn = $(e.currentTarget);
+      $btn.addClass('hover');
+      clearTimeout(this.closeButton);
+      this.closeButton = setTimeout(function(){
+        $btn.removeClass('hover');
+      }, 2000);
+    }
+
+  }
+
+	$().ready(function(){
+  	Core.init();
+	});
+
+})(window.jQuery || window.Zepto);
